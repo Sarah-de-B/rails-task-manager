c16465c4af5051c73f122529940c106f7647ba91
sarah crosnier de bellaistre <sarah.debellaistre@audencia.com>
2025-08-12 14:58:05 +0200

    app todo

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "list", to: "tasks#list"
+  get "tasks/:id", to: "tasks#show", as: "task"
+  get "tasks", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#remove", as: "remove_task"
 end
